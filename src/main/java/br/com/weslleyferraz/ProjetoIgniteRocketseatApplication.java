13d084f40e9a6d3c878700d7ba225ab1c2c1ed00
Weslley H V Ferraz <weslleyhenrique800@gmail.com>
2024-01-15 06:18:33 -0400

    a

---
+++ src/main/java/br/com/weslleyferraz/ProjetoIgniteRocketseatApplication.java
@@ -0,0 +1,13 @@
+package br.com.weslleyferraz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIgniteRocketseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIgniteRocketseatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/weslleyferraz/ProjetoIgniteRocketseatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weslleyferraz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIgniteRocketseatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
