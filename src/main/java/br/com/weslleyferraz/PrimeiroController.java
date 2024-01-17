43644f7e2cc7cfa942f23568ca2bc36815c9ba71
Weslley H V Ferraz <weslleyhenrique800@gmail.com>
2024-01-17 06:49:56 -0400

    a

---
+++ src/main/java/br/com/weslleyferraz/PrimeiroController.java
@@ -0,0 +1,9 @@
+package br.com.weslleyferraz;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraController")
+public class PrimeiroController {
+}
